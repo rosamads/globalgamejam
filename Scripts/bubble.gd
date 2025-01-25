1ce0b6834d844c7a80a7806ff6c82930c28cb16d
“Rosa <149009665+soggyrosa@users.noreply.github.com>
2025-01-25 13:21:11 -0400

    RAHHH le bobble

--- Scripts/bubble.gd
+++ Scripts/bubble.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
+@onready var sprite = $AnimatedSprite2D
+
 const air_friction_multiplier = 2.5
+
+func _ready() -> void:
+	sprite.play("spawn")
 
 func _physics_process(delta: float) -> void:
 	velocity = velocity - velocity * air_friction_multiplier * delta
-	
 	move_and_slide()
 
 
 func _on_collision_timer_timeout() -> void:
 	collision_layer = 0b100
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	sprite.play("default")
