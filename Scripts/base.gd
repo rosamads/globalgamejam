a33978bfe159239084d7f5ab49eb4c7437dadae2
Jamie Pothier <jm318457@dal.ca>
2025-01-25 10:29:54 -0400

    Camera movement scripts finished

--- Scripts/base.gd
+++ Scripts/base.gd
@@ -1,5 +1,10 @@
 extends Node
 
+
+const cam_move_multiplier = 1.2
+const cam_reset_speed = 25
+const look_ahead_multiplier = 100
+
 var maincam: Camera2D
 var player: CharacterBody2D
 
@@ -12,4 +17,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	maincam.position = player.position
+	if player.velocity.x != 0:
+		#cam x
+		var x_dir = 1 if player.velocity.x > 0 else -1
+		var look_ahead_x = player.position.x + (x_dir * look_ahead_multiplier)
+		maincam.position.x = move_toward(maincam.position.x,
+			look_ahead_x, abs(player.velocity.x) * cam_move_multiplier * delta)
+	else:
+		maincam.position.x = move_toward(maincam.position.x,
+			player.position.x, cam_reset_speed * delta)
+	
+	if player.velocity.y != 0:
+		#cam y
+		var y_dir = 1 if player.velocity.y > 0 else -1
+		var look_ahead_y = player.position.y + (y_dir * look_ahead_multiplier)
+		maincam.position.y = move_toward(maincam.position.y,
+			look_ahead_y, abs(player.velocity.y) * cam_move_multiplier * delta)
+	else:
+		maincam.position.y = move_toward(maincam.position.y,
+			player.position.y, cam_reset_speed * delta)
