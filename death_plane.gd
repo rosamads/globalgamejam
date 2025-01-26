df268693d270083ca62a30d31d720aeeb314e0b8
Jamie Pothier <jm318457@dal.ca>
2025-01-26 14:15:28 -0400

    checkpoints

--- death_plane.gd
+++ death_plane.gd
@@ -1,19 +1,4 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_death_plane_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-
 func _on_body_entered(body: Node2D) -> void:
 	body.pop_self()
